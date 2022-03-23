48caf4d6628c5fe6f5f7bd3d3e84ef592fe5673d
tanyathegreen <roldugtanya@yandex.ru>
2022-03-23 14:19:59 +0300

    Решение_v3

--- task_2/solution.js
+++ task_2/solution.js
@@ -35,7 +35,7 @@ function calcDiscount(sum, min, discount) {
     let discountPart = discount; // величина скидки в процентах
     let discountSum;
     if (productsSum >= discountMinSum) {
-        discountSum = discountPart / 100 * productsSum;
+        discountSum = discountPart / (100 * productsSum);
     }
     else {
         discountSum = 0;
