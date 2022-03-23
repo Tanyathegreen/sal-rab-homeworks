a78ba3cbff9ca8d34274e998d5318c0a07eb3d11
tanyathegreen <roldugtanya@yandex.ru>
2022-03-23 15:09:25 +0300

    Решение_v3

--- task_2/solution.js
+++ task_2/solution.js
@@ -3,10 +3,10 @@ function calcShipping(sum, min, shipping) {
     let freeShippingMinSum = min; // минимальная цена для бесплатной доставки
     let shippingPrice = shipping; // стоимость доставки
     let shippingSum;
-    if (productsSum == 0 && productsSum >= freeShippingMinSum) {
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
         shippingSum = 0;
     }
-    else if (productsSum > 0 && productsSum < freeShippingMinSum); {
+    else if (productsSum >= 0 && productsSum <= freeShippingMinSum); {
         shippingSum = shippingPrice;
     }
 
