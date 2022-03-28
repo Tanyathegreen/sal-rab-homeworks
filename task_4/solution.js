acc88b60882c2b0febbc07d1df1ae217e7e4d5e1
tanyathegreen <roldugtanya@yandex.ru>
2022-03-28 12:46:40 +0300

    Решение_v1

--- task_4/solution.js
+++ task_4/solution.js
@@ -16,8 +16,8 @@ function parseProducts(json) {
 function renderProductsCards(json){
     clearProducts();
     let products = parseProducts(json);
-    let length = products.length;
-    for (let i = 0; i < length; i += 1){
+    length = products.length;
+    for (let i = 0; i < length; i += 1) {
         addProducts(products[i]);
     }
 }
