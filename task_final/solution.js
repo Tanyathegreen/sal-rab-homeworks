4984f759760cb65f2c6dc4344629b069ebe8e912
tanyathegreen <roldugtanya@yandex.ru>
2022-03-29 12:20:20 +0300

    Решение_v1

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,7 +34,9 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {client: {name, phone}, goods: [{title, count}], order: {address, sum}};
+    const obj = {data: data};
+    
 
     let countOfGoods = goods.length;
 
